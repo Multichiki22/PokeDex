c0ae4e5d2e4e0b9cd030a184984cb63c62e985eb
Multichiki22 <myguelangel12@gmail.com>
2023-08-10 20:01:33 -0500

    Remove unnecessary line

--- api/src/app.js
+++ api/src/app.js
@@ -16,7 +16,6 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', "*"); 
-  // res.header('Access-Control-Allow-Origin', 'http://192.168.1.4');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
